04b6b39615fa3b5d23fd1a92fefb9c4149835ca9
ronak <ronak@DESKTOP-8PSVOJI>
2021-08-07 00:23:52 +0530

    Add project files.

---
+++ UserProfile/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UserProfile;
+using UserProfile.Models;
+
+namespace UserProfile.Controllers
+{
+    public class HomeController : Controller
+    {
+        private PersonalInfoEntities1 db = new PersonalInfoEntities1();
+        public ActionResult Index()
+        {
+            var userProfileHeaders = db.UserProfileHeaders;
+            return View(userProfileHeaders.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            UserProfileData upData = new UserProfileData();
+            return View(upData);
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(UserProfileData userProfileHeader)
+        {
+            if (ModelState.IsValid)
+            {
+                var header = new UserProfileHeader
+                {
+                    Address = userProfileHeader.Address,
+                    Name = userProfileHeader.Name,
+                    Age = userProfileHeader.Age,
+                    Country = userProfileHeader.Country,
+                    State = userProfileHeader.State,
+                    City = userProfileHeader.City,
+                    Gender = userProfileHeader.Gender
+
+                };
+
+
+
+                db.UserProfileHeaders.Add(header);
+                db.SaveChanges();
+                if (header.UserId > 0)
+                {
+                    if (userProfileHeader.UserProfileDetails.ExperianceCompany != null || userProfileHeader.UserProfileDetails.CompanyName != null || userProfileHeader.UserProfileDetails.Designation != null)
+                    {
+                        var detail = new UserProfileDetail
+                        {
+                            UserId = header.UserId,
+                            ExperianceCompany = userProfileHeader.UserProfileDetails.ExperianceCompany,
+                            CompanyName = userProfileHeader.UserProfileDetails.CompanyName,
+                            Designation = userProfileHeader.UserProfileDetails.Designation
+                        };
+                        db.UserProfileDetails.Add(detail);
+                        db.SaveChanges();
+                    }
+                    ViewBag.Success = "Inserted";
+
+                }
+                ModelState.Clear();
+            }
+            return View();
+        }
+
+        public JsonResult GetSearchingData(string SearchBy, string SearchValue)
+        {
+            List<UserProfileHeader> userList = new List<UserProfileHeader>();
+            if (SearchBy == "ID")
+            {
+                try
+                {
+                    int Id = Convert.ToInt32(SearchValue);
+                    userList = db.UserProfileHeaders.Where(x => x.UserId == Id || SearchValue == null).ToList();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("{0} Is Not A ID ", SearchValue);
+                }
+                return Json(userList, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                userList = db.UserProfileHeaders.Where(x => x.Name.StartsWith(SearchValue) || SearchValue == null).ToList();
+                return Json(userList, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ UserProfile/Models/UserProfile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UserProfile.Models
+{
+    public class UserProfileData
+    {
+
+        public int UserId { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(10)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Age is required")]
+        public Nullable<int> Age { get; set; }
+        [Required(ErrorMessage = "Gender is required")]
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Country is required")]
+        public string Country { get; set; }
+        [Required(ErrorMessage = "State is required")]
+        public string State { get; set; }
+        [Required(ErrorMessage = "City is required")]
+        public string City { get; set; }
+        public string CreatedBy { get; set; }
+        public Nullable<System.DateTime> CreatedOn { get; set; }
+
+        public UserProfileDetailData UserProfileDetails { get; set; }
+    }
+
+    public class UserProfileDetailData
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string ExperianceCompany { get; set; }
+        public string CompanyName { get; set; }
+        public string Designation { get; set; }
+        public string CreatedBy { get; set; }
+        public Nullable<System.DateTime> CreatedOn { get; set; }
+    }
+}
